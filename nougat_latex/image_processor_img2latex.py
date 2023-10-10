737d65865a5def02834309c905cae9b19cfc3690
NormXU <xunuo@datagrand.com>
2023-10-10 17:43:49 +0800

    reformat processor

--- nougat_latex/image_processor_img2latex.py
+++ nougat_latex/image_processor_img2latex.py
@@ -2,17 +2,21 @@
 # create: @time: 9/22/23 16:11
 
 from PIL import Image
+from .image_processing_nougat import NougatImageProcessor
 
+class NougatLaTexProcessor(NougatImageProcessor):
 
-class NougatLaTexProcessor:
-
-    def __init__(self, image_processor, img_height=224, img_width=560):
+    def __init__(self, img_height=224, img_width=560, **kwargs):
         self.imgH = img_height
         self.imgW = img_width
         self.maxH = img_height // 2
-        self.image_processor = image_processor
+        super(NougatLaTexProcessor, self).__init__(**kwargs)
 
     def __call__(self, images, **kwargs):
+        images = self._rescale(images)
+        return self.preprocess(images, **kwargs)
+
+    def _rescale(self, images):
         if images.height < self.maxH:
             empty_img = Image.new("RGB", (self.imgW, self.imgH))
             target_w = max(1, int(images.width / images.height * self.maxH))
@@ -26,4 +30,4 @@ class NougatLaTexProcessor:
             start_w = 0
             empty_img.paste(images, (start_w, start_h))
             images = empty_img
-        return self.image_processor(images, return_tensors="pt", **kwargs).pixel_values
+        return images
